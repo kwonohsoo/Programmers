d70c396a3df0bf37f947ec5fd448aeb28e269df9
kwon524 <kwon524@naver.com>
2023-01-09 21:37:27 +0900

    LV0

---
+++ Programmers_LV0/src/dev/LV0_저주의_숫자_3.java
@@ -0,0 +1,18 @@
+package dev;
+
+public class LV0_저주의_숫자_3 {
+
+	public static void main(String[] args) {
+		int n = 15;
+		int answer = 0;
+
+		for (int i = 0; i < n; i++) {
+			answer++;
+			while (answer % 3 == 0 || String.valueOf(answer).contains("3")) {
+				answer++;
+			}
+		}
+		System.out.println(answer);
+	}
+
+}
